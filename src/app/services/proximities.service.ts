10fbf7945b2e647b643a8705810d399a2a1f1464
Manuel Poell <manuelpoell@gmx.at>
2023-10-14 23:24:02 +0200

    hide tokens if inside fog

--- src/app/services/proximities.service.ts
+++ src/app/services/proximities.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import OBR, { Item } from '@owlbear-rodeo/sdk';
+import OBR, { Item, Vector2 } from '@owlbear-rodeo/sdk';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Proximity } from '../models/proximity.model';
 
@@ -17,8 +17,9 @@ export class ProximitiesService {
     const currentMeasurement = await OBR.scene.grid.getMeasurement();
     const scale = await OBR.scene.grid.getScale();
     await OBR.scene.grid.setMeasurement('EUCLIDEAN');
+    const fogs = await OBR.scene.items.getItems((item) => item.layer === 'FOG');
     for (let token of characters) {
-      if (!token.visible || token.id === this.referenceToken.id) continue;
+      if (this.invisibleToken(token, fogs)) continue;
       const distance = await OBR.scene.grid.getDistance(this.referenceToken.position, token.position);
       proximities.push({
         name: token.name,
@@ -45,5 +46,42 @@ export class ProximitiesService {
 
   getReferenceTokenStream(): Observable<Item | null> {
     return this.referenceToken$.asObservable();
+  }
+
+  private invisibleToken(token: Item, fogs: Array<Item>): boolean {
+    if (!this.referenceToken) return true;
+    const insideFogOfWar: Array<boolean> = fogs.map((fog: any) =>
+      this.isInPolygon(
+        this.convertPosition(token.position),
+        this.convertToPolygon(this.convertPosition(fog.position), fog['commands'])
+      )
+    );
+    return insideFogOfWar.some((inside) => inside) || !token.visible || token.id === this.referenceToken.id;
+  }
+
+  // https://stackoverflow.com/a/29915728
+  private isInPolygon(point: Array<number>, vs: Array<Array<number>>): boolean {
+    const x = point[0];
+    const y = point[1];
+
+    let inside = false;
+    for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+      const xi = vs[i][0];
+      const yi = vs[i][1];
+      const xj = vs[j][0];
+      const yj = vs[j][1];
+
+      const intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+      if (intersect) inside = !inside;
+    }
+    return inside;
+  }
+
+  private convertPosition(position: Vector2): Array<number> {
+    return [position.x, position.y];
+  }
+
+  private convertToPolygon(start: Array<number>, commands: Array<Array<number>>): Array<Array<number>> {
+    return commands.map((cmd) => [start[0] + cmd[1], start[1] + cmd[2]]).slice(0, -1);
   }
 }
